97daea8e53846e784804e9434b37c1aa4d0fbb21
Frances Cancio <frances.cancio@gmail.com>
2019-10-09 11:25:27 -0500

    creates functional CircleSelector based on handleClick

---
+++ src/components/Circles/Circles.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Circles.css';
+
+const Circles = () => {
+    return (
+        <div>
+            Circles
+        </div>
+    )
+}
+
+export default Circles;
